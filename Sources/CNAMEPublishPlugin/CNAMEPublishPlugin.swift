0117c08bc7f97e6ffc8247604ac14a1f976f231b
Emanuel Guerrero <emanuelguerrerodev@gmail.com>
2020-03-10 22:59:13 -0400

    Added dedicated error type for representing possible errors

--- Sources/CNAMEPublishPlugin/CNAMEPublishPlugin.swift
+++ Sources/CNAMEPublishPlugin/CNAMEPublishPlugin.swift
@@ -5,7 +5,31 @@
 */
 
 import Files
+import Foundation
 import Publish
+
+// MARK: - CNAME_Error
+
+/// Supported errors that can occur with CNAME file generation.
+public enum CNAME_Error: LocalizedError {
+    
+    /// The list of provided domain names is empty.
+    case listEmpty
+    
+    /// In the list of provided domain names, there is an empty string.
+    case containsEmptyString
+    
+    public var localizedDescription: String {
+        switch self {
+        case .listEmpty:
+            return "The provided list of domain names are empty. At least one domain name is required for file generation."
+        case .containsEmptyString:
+            return "One of the provided domain names is an empty string."
+        }
+    }
+}
+
+// MARK: - Plugin
 
 public extension Plugin {
     
@@ -23,10 +47,16 @@ public extension Plugin {
         }
     }
     
+    static func generateCNAME(with domainNames: [String]) -> Self {
+        Plugin(name: "Generate CNAME from provided domain names") { context in
+            guard domainNames.allSatisfy({ !$0.isEmpty }) else { return }
+        }
+    }
+    
     /// Returns a plugin that copies a custom domain name file from the website's resources directory to the website's
     /// output directory.
     static func addCNAME() -> Self {
-        Plugin(name: "Generate CNAME for custom domain names") { context in
+        Plugin(name: "Generate CNAME from existing CNAME file in the Resources directory") { context in
             let file = try context.file(at: "Resources/CNAME")
             
             let fileContent = try file.readAsString()
